8d66174dc24f1f8a15605303e6cca9b7e8fc0d79
mlehr3 <mlehr3@outlook.com>
2023-09-27 10:05:51 -0400

    added string literal

--- index.js
+++ index.js
@@ -3,21 +3,31 @@ let myCaptures = ["www.awesomelead.com", "www.epiclead.com", "www.greatlead.com"
 
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
-// 2. Grab the unordered list and store it in a const variable called ulEl
 const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myCaptures.push(inputEl.value)
+    // Added functionality of clearing input box after input button press
+    inputEl.value = ""
     console.log(myCaptures)
     renderLeads()
 })
 
-// Create function renderLeads to list out newly added inputs
 function renderLeads() {
     // Dom manipulation comes with a cost on speed, try to miinimize document manipulatiosn
     let listItems = ""
     for (i = 0; i < myCaptures.length; i ++) {
-        listItems += "<li>" + myCaptures[i] + "</li>"
+        // Added functionality of being able to click links with <a> tag
+        // listItems += '<li><a target="_blank" href="'+ myCaptures[i] + '">' + myCaptures[i] + "</a></li>"
+        // Use `` backticks to make template strings, looks a lot cleaner
+            // or template literals
+        listItems += `
+            <li>
+                <a target="_blank" href="${myCaptures[i]}">
+                    ${myCaptures[i]}
+                </a>
+            </li>
+        `
     }
     ulEl.innerHTML = listItems
     console.log(listItems)
